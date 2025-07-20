46b80566c7386c55b2a713b1cbe63582780fbca6
lyydsheep <2230561977@qq.com>
2025-07-20 18:30:14 +0800

    Initial commit

--- k8s-combat.go
+++ k8s-combat.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("pong")
-		fmt.Fprint(writer, "pong")
+		name, _ := os.Hostname()
+		log.Println(name, "ping")
+		fmt.Fprint(writer, name+" pong")
+	})
+	http.HandleFunc("/service", func(writer http.ResponseWriter, request *http.Request) {
+		// 对应的服务
+		resp, _ := http.Get("http://k8s-combat-service:8080/ping")
+		body, _ := io.ReadAll(resp.Body)
+		log.Println(string(body))
+		fmt.Fprint(writer, string(body))
 	})
 	http.ListenAndServe(":8080", nil)
 }
